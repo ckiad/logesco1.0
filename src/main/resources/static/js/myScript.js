6ee0f89124b3064178911457f72b93a71957777e
kiadjeuc <ckiadjeu@gmail.com>
2019-01-01 14:54:41 +0100

    second commit concernant l'amelioration de l'interface: creation du fichier broullon menu contenant l'ancien menu qui etait fait de bouton dropdown

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -2,9 +2,18 @@
  * 
  */
 
-$(document).ready(function() {
-	//alert("Enfin on pourra tester un peu de Ajax");
-});
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel-menu').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	} else {
+		$this.parents('.panel-menu').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	}
+})
 
 /*function enreg(){
 	alert("ici");
